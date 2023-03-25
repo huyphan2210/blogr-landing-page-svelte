3a5422f743b4159430c22b30e2bb10fc869b3655
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-25 17:09:14 +0700

    Create Footer.svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+</script>
+<footer>
+  Product
+
+  Overview
+  Pricing
+  Marketplace
+  Features
+  Integrations
+
+  Company
+
+  About
+  Team
+  Blog
+  Careers
+
+  Connect
+  
+  Contact
+  Newsletter
+  LinkedIn
+  <div id='challenger'>
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/huyphan2210" target="_blank">Huy Phan</a>.
+  </div>
+</footer>
+<style scoped>
+
+</style>
