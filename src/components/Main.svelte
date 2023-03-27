1886c8778eacfe222a6704e27646727f1941434f
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-27 14:43:40 +0700

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -11,32 +11,55 @@
             <button type="button">Learn more</button>
         </div>
     </section>
-  
-
-  Designed for the future
-
-  Introducing an extensible editor
-  Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
-  The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
-  videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
-  change the looks of a blog.
-
-  Robust content management
-  Flexible content management enables users to easily move through posts. Increase the usability of your blog 
-  by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
-
-  State of the Art Infrastructure
-  With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
-  This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
-
-  Free, open, simple
-  Blogr is a free and open source application backed by a large community of helpful developers. It supports 
-  features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
-  and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
-
-  Powerful tooling
-  Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
-  capable of producing even the most complicated sites.
+    <section id='design'>
+        <h1>Designed for the future</h1>
+        <article>
+            <div>
+                <h2>Introducing an extensible editor</h2>
+                <p>
+                    Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                    The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
+                    videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
+                    change the looks of a blog.
+                </p>
+            </div>
+            <div>
+                <h2>Robust content management</h2>
+                <p>
+                    Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                    by adding customized categories, sections, format, or flow. With this functionality, you're in full control.
+                </p>
+            </div>
+        </article>
+    </section>
+    <section id='art'>
+        <article>
+            <h2>State of the Art Infrastructure</h2>
+            <p>
+                With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
+                This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
+            </p>
+        </article>
+    </section>
+    <section id='reason'>
+        <article>
+            <div>
+                <h2>Free, open, simple</h2>
+                <p>
+                    Blogr is a free and open source application backed by a large community of helpful developers. It supports 
+                    features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
+                    and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
+                </p>
+            </div>
+            <div>
+                <h2>Powerful tooling</h2>
+                <p>
+                    Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
+                    capable of producing even the most complicated sites.
+                </p>
+            </div>
+        </article>
+    </section>
 </main>
 <style scoped>
     #hero {
@@ -47,13 +70,15 @@
         background-image: linear-gradient(to right, #ff8b70, #ff4457);
         height: 100vh;
         overflow: hidden;
+        border-radius: 0 0 0 10rem;
     }
         #hero img {
             position: absolute;
             top: 50%;
             left: 0;
-            transform: translate(-10%, -50%);
+            transform: translate(0, -50%);
             z-index: 0;
+            max-width: 150%;
         }
 
         #hero div {
@@ -65,4 +90,33 @@
             #hero div h1 {
                 font-size: clamp(2rem, 4vw, 4rem);
             }
+            #hero div button {
+                font-family: Ubuntu, sans-serif;
+                font-weight: 600;
+                padding: 1rem 2rem;
+                border-style: solid;
+                border-color: white;
+                border-radius: 2rem;
+                border-width: 0.1rem;
+                color: var(--light-red);
+                font-size: 1rem;
+            }
+                #hero div button:last-child {
+                    background-color: transparent;
+                    color: white;
+                }
+            #hero div button:hover {
+                cursor: pointer;
+            }
+
+@media screen and (max-aspect-ratio: 1) {
+    #hero {
+        background-image: linear-gradient(135deg, #ff8b70, #ff4457);
+    }
+    #hero img {
+        left: 50%;
+        transform: translate(-45%, -45%);
+        max-width: 400%;
+    }
+}
 </style>
