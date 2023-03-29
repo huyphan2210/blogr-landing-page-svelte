0cde86323c48c46dea0a8a444cbb6df723252194
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-29 09:18:33 +0700

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import heroImg from '../assets/images/bg-pattern-intro-desktop.svg'
+    import editorDesktop from '../assets/images/illustration-editor-desktop.svg'
+
+    import phone from '../assets/images/illustration-phones.svg'
 </script>
 <main>
     <section id='hero'>
@@ -13,28 +16,36 @@
     </section>
     <section id='design'>
         <h1>Designed for the future</h1>
-        <article>
-            <div>
-                <h2>Introducing an extensible editor</h2>
-                <p>
-                    Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
-                    The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
-                    videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
-                    change the looks of a blog.
-                </p>
+        <div class='paragraph'>
+            <article>
+                <div>
+                    <h2>Introducing an extensible editor</h2>
+                    <p>
+                        Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                        The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
+                        videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
+                        change the looks of a blog.
+                    </p>
+                </div>
+                <div>
+                    <h2>Robust content management</h2>
+                    <p>
+                        Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                        by adding customized categories, sections, format, or flow. With this functionality, you're in full control.
+                    </p>
+                </div>
+            </article>
+            <div class='backgroundImg'>
+                <img src={editorDesktop} alt='Editor'>
             </div>
-            <div>
-                <h2>Robust content management</h2>
-                <p>
-                    Flexible content management enables users to easily move through posts. Increase the usability of your blog 
-                    by adding customized categories, sections, format, or flow. With this functionality, you're in full control.
-                </p>
-            </div>
-        </article>
+        </div>
     </section>
     <section id='art'>
+        <div>
+            <img src={phone} alt='Phone'>
+        </div>
         <article>
-            <h2>State of the Art Infrastructure</h2>
+            <h1>State of the Art Infrastructure</h1>
             <p>
                 With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
                 This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
@@ -107,6 +118,77 @@
                 }
             #hero div button:hover {
                 cursor: pointer;
+            }
+    #design {
+        position: relative;
+    }
+        #design h1 {
+            color: var(--very-dark-blue);
+            position: absolute;
+            top: 0rem;
+            left: 50%;
+            transform: translate(-50%);
+            text-align: center;
+            font-size: clamp(2rem, 3vw, 3rem);
+        }
+
+    .paragraph {
+        margin-block-start: 8rem;
+        display: grid;
+        grid-template-columns: 50% 50%;
+    }
+        .paragraph article {
+            padding: 15%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+            .paragraph article h2 {
+                color: var(--very-dark-blue);
+                font-size: clamp(1.5rem, 2vw, 2rem);
+            }
+            .paragraph article p {
+                color: var(--very-dark-grayish-blue);
+                line-height: 2rem;
+            }
+
+        .backgroundImg {
+            position: relative;
+        }
+            .backgroundImg img {
+                width: 130%;
+                transform: translate(3%);
+            }
+
+    #art {
+        display: grid;
+        justify-content: center;
+        grid-template-columns: 50% 50%;
+        color: white;
+        background-image: linear-gradient(to right, hsl(237, 17%, 21%), hsl(237, 23%, 32%));
+        border-radius: 0 8rem 0 2rem;
+        margin-block: 5rem;
+    }
+        #art article {
+            padding: 15% 15% 15% 0;
+        }
+            #art article h1 {
+                margin-block-start: 0;
+                font-size: clamp(2rem, 3vw, 3rem);
+            }
+            #art article p {
+                line-height: 2rem;
+                margin-block-end: 0;
+            }
+        #art div{
+            overflow: visible;
+            position: relative;
+        }
+            #art div img {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
             }
 
 @media screen and (max-aspect-ratio: 1) {
